8e53d2bdd88490ff3ff9e2f4866e75721149cdd1
jvitor03 <j_vitor03@yahoo.com.br>
2013-05-24 01:01:47 -0300

    bafo

---
+++ bafo.c
@@ -0,0 +1,45 @@
+//feito
+#include<stdio.h>
+int main (void)
+{
+   int rodada;
+      int cont;
+      int totala=0,totalb=0;
+      int a;
+      int b;
+      int testen=0;
+  while (1)
+    {
+      //char x[4];
+      testen = testen+1;
+     
+      scanf("%d",&rodada);
+      if (rodada == 0)
+	{
+	  break;
+	}
+      cont = 0;
+      totala = 0;
+      totalb = 0;
+      while (cont < rodada)
+      {
+	scanf("%d",&a);
+	scanf("%d",&b);
+	totala = a+totala;
+	totalb = b+totalb;
+	//printf("%d %d\n",totala,totalb);
+	cont++;
+      }
+      if (totala > totalb)
+		    {
+
+		      printf("Teste %d\nAldo\n\n",testen);
+		    }
+      if (totalb > totala)
+		    {
+		      printf("Teste %d\nBeto\n\n",testen);
+		    }
+    }
+
+  return 0;
+}
